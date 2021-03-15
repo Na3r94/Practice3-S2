1d0b4fd8113717a923c0746db0c821f1e4330ef3
Naser Gholami <80551568+Na3r94@users.noreply.github.com>
2021-03-16 03:05:18 +0330

    Add files via upload

---
+++ B.M.M.py
@@ -0,0 +1,22 @@
+
+Num1 = int(input())
+Num2 = int(input())
+
+A = []
+B = []
+
+if Num1 > Num2:
+    Num3 = Num1
+
+else:
+    Num3 = Num2
+
+for i in range(1,Num3+1):
+    if Num1 % i == 0 and Num2 % i == 0:
+        A.append(i)
+
+
+print(max(A))
+
+
+
